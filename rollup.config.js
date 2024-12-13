677988f31d0ef1c6639e14fdac3fbca1f716cdf3
zhaijingjing <495270674@qq.com>
2024-12-13 09:49:59 +0800

    fix: 打包优化，导出为esm

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import alias from "@rollup/plugin-alias";
 import babel from "rollup-plugin-babel";
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dynamicImportVars from "@rollup/plugin-dynamic-import-vars";
 import path from "path";
@@ -11,10 +11,14 @@ export default {
   input: "src/index.ts", // 入口文件
   output: {
     dir: "lib", // 输出文件
-    format: "umd",
-    name: "WeTree",
+    format: "esm",
     //当入口文件有export时，'umd'格式必须指定name
     //这样，在通过<script>标签引入时，才能通过name访问到export的内容。
+    name: "WeTree",
+    exports: "named", // 指定导出模式（自动、默认、命名、无）
+    preserveModules: true, // 保留模块结构
+    preserveModulesRoot: "src", // 将保留的模块放在根级别的此路径下
+    sourcemap: true, // 启用源映射
   },
   plugins: [
     resolve(),
@@ -23,7 +27,11 @@ export default {
       entries: [{ find: "@/src", replacement: path.resolve(__dirname, "src") }],
     }),
     typescript({
-      tsconfig: "./tsconfig.json",
+      // tsconfig: "./tsconfig.json",
+      outDir: "lib",
+      declaration: true,
+      declarationDir: "lib",
+      sourceMap: true, // 确保 TypeScript 插件也生成源映射
     }),
     babel({
       exclude: "node_modules/**", // 防止打包node_modules下的文件
